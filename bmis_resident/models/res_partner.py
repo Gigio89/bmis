f8f9e36c677b8ff49ecfc2af3e9e3514f45d62be
Gigio89 <it@infinion.ch>
2024-05-07 01:43:59 +0000

    [Add] Contacts Module

--- bmis_resident/models/res_partner.py
+++ bmis_resident/models/res_partner.py
@@ -11,3 +11,4 @@ class Partner(models.Model):
     first_name = fields.Char(string="First Name")
     last_name = fields.Char(string="Last Name")
     middle_name = fields.Char(string="Middle Name")
+    company_type = fields.Selection(selection_add=[('member','Member'),('head','Head')])
