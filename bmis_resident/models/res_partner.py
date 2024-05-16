e597be1fd6f62cd4efe7aad4ed4fe9b9418b291a
Gigio89 <it@infinion.ch>
2024-05-16 08:55:10 +0000

    [UPDATE]

--- bmis_resident/models/res_partner.py
+++ bmis_resident/models/res_partner.py
@@ -2,13 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, models, modules,fields
-from datetime import date
+from datetime import date,datetime,timedelta
+from dateutil.relativedelta import relativedelta
 
 class ResPartner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
-    name = fields.Char()
     first_name = fields.Char(string="First Name",required=True)
     last_name = fields.Char(string="Last Name",required=True)
     middle_name = fields.Char(string="Middle Name")
@@ -18,7 +18,7 @@ class ResPartner(models.Model):
     street_id = fields.Many2one(string="Street",comodel_name="bmis.street")
     village_subdivision_id = fields.Many2one(string="Village/Subdibvision",related="street_id.village_subdivision_id")
     barangay_id = fields.Many2one(string="Barangay",related="street_id.barangay_id")
-    city_municipality_id = fields.Many2one(string="City-Municipality",related="barangay_id.city_municipality_id",readonly="1")
+    city_municipality_id = fields.Many2one(string="City-Municipality",related="barangay_id.city_municipality_id",readonly=True)
     province_id = fields.Many2one(string="Province",related="city_municipality_id.province_id")
     country_id = fields.Many2one(string="Country", related="province_id.country_id")
     street2 = fields.Char(related="barangay_id.name",string="Barangay")
@@ -28,11 +28,20 @@ class ResPartner(models.Model):
     birthday = fields.Date(string="Birthday",comodel_name="bmis_resident.personal_detail")
     civil_status = fields.Selection(string="Civil Status",selection=[('single','Single'),('married','Married'),('widowed','Widowed')], comodel_name="bmis_resident.personal_detail")
     gender = fields.Selection(string="Gender", comodel_name="bmis_resident.personal_detail",selection=[('male','Male'),('female','Female')])
-    age = fields.Integer(string="Age",comodel_name="bmis_resident.personal_detail")
+    age = fields.Integer(string="Age",store=True, readonly=True)
+    test = fields.Char(string="test",store=True)
     
-
-    api.onchange("first_name","middle_name","last_name")
+    
+    @api.onchange("first_name","last_name")
     def _onchange_first_name(self):
         for record in self:
             if record.first_name and record.last_name:
-                record['name'] = str(record.first_name) + ' ' + str(record.last_name)    
+                record.name = str(record.first_name) + ' ' + str(record.last_name)
+                
+    @api.onchange("birthday")
+    def _get_age(self):
+        for record in self:
+            if record.birthday:
+                bd = record.birthday
+                age = date.today().year - bd.year
+                record.age = str(age)
